452227532152aad51be4ea75dab19a91ec70e7a4
dolly2001 <piyushsolanki18dec@gmail.com>
2024-05-31 01:33:13 +0530

    header responsive

--- index.js
+++ index.js
@@ -3,3 +3,15 @@ document.addEventListener('DOMContentLoaded', () => {
     console.log('DOM fully loaded and parsed');
     // Add any JavaScript interactivity here
 });
+
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    const menuToggle = document.getElementById('menu-toggle');
+    const navUl = document.querySelector('nav ul');
+
+    menuToggle.addEventListener('click', () => {
+        navUl.classList.toggle('active');
+        menuToggle.classList.toggle('active');
+    });
+});
